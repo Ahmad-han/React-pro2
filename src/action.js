55570a2cd792aae27ea323a312fdb769fb8da81e
Ahmad <khan@mail.ru>
2023-06-22 01:09:45 +0300

    creating second react-pro task

---
+++ src/action.js
@@ -0,0 +1,15 @@
+export const loadPosts = () => {
+    return (dispatch) => {
+
+        dispatch({type: "load/posts/start"})
+
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((json) => {
+            dispatch({
+                type: "load/posts/fulfilled",
+                payload: json
+            })
+        })
+    }
+}
